43ee1566e320e1b6504c6220f637e4f367e6cac3
Olivier02500 <olivier.peuchet@hotmail.com>
2021-11-17 19:23:29 +0100

    make exo 69

--- exo 2/app.js
+++ exo 2/app.js
@@ -4,12 +4,17 @@ variable
 let liste = document.getElementById('liste-commissions');
 let text = document.getElementById('text');
 let submit = document.getElementById('button');
+let supp = document.getElementById('delete');
 
 
 submit.addEventListener('click', function() {
     let creatLi = document.createElement('li');
     creatLi.innerHTML = text.value;
     liste.appendChild(creatLi);
+})
+
+supp.addEventListener('click',function(){
+    liste.lastChild.remove();
 })
 
 
